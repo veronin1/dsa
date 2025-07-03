0ccafaf1e0ce5d333b33e739512035f4516c663c
Harry Newman <h.newman2005@outlook.com>
2025-07-03 20:22:16 +0100

    Moved main logic to a different file and kept linkedlist.c for definitions only

--- data structures/c/linked_list/linkedlist.c
+++ data structures/c/linked_list/linkedlist.c
@@ -4,53 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-  LinkedList* list = createLinkedList();
-
-  pushFront(list, 2);  // List: 2
-  pushFront(list, 99); // List: 99, 2
-  pushBack(list, 3);   // List: 99, 2, 3
-  pushBack(list, 5);   // List: 99, 2, 3, 5
-
-  printf("Initial list:\n");
-  printList(list);
-
-  printf("Front element: %d\n", front(list));
-  printf("Back element: %d\n", back(list));
-
-  printf("Pop front: %d\n", popFront(list));
-  printf("Pop back: %d\n", popBack(list));
-
-  printf("List after popping front and back:\n");
-  printList(list);
-
-  printf("Size: %d\n", size(list));
-  printf("Is empty? %d\n", empty(list));
-
-  // Add a few more elements
-  pushBack(list, 10);
-  pushBack(list, 20);
-  pushBack(list, 30);
-
-  printf("List before removing value 20:\n");
-  printList(list);
-
-  removeValue(list, 20);
-
-  printf("List after removing value 20:\n");
-  printList(list);
-
-  clear(list);
-  printf("List after clear:\n");
-  printList(list);
-
-  printf("Is empty after clear? %d\n", empty(list));
-
-  free(list);
-
-  return 0;
-}
-
 LinkedList* createLinkedList() {
   LinkedList* list = malloc(sizeof(LinkedList));
   if (list == NULL) {
