5c06fdd9127269cd4241af695ac9f49e8b9b2af5
Harry Newman <h.newman2005@outlook.com>
2025-07-03 18:26:08 +0100

    Added pop_back: Removes and returns the last element from the linked list.

--- data structures/python/linked_list/linked_list.py
+++ data structures/python/linked_list/linked_list.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self, head, size):
         self.head = None
@@ -11,10 +12,6 @@ class LinkedList:
     # Adds the element `value` to the end of the linked list.
     def push_back(self, value):
         new_node = Node(value)
-        if not new_node:
-            return
-        new_node.data = value
-        new_node.next = None
 
         if not self.head:
             self.head = new_node
@@ -22,27 +19,40 @@ class LinkedList:
             current = self.head
             while current.next is not None:
                 current = current.next
-            
             current.next = new_node
-        
+
         self.size += 1
+        return
 
     # Inserts the element `value` at the beginning of the linked list
     def push_front(self, value):
         new_node = Node(value)
-        if not new_node:
-            return
-        new_node.data = value
         new_node.next = self.head
         self.head = new_node
         self.size += 1
+        return
 
     def print_list(self):
         current = self.head
         while current is not None:
             print(current.data)
+            current = current.next
+        return
 
+    def pop_back(self):
+        if not self.head:
+            return
 
+        if self.head.next is None:
+            self.head = None
+            return
+
+        current = self.head
+        while current.next.next is not None:
+            current = current.next
+        current.next = None
+        self.size -= 1
+        return
 
 
 def main():
