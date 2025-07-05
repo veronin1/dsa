b9b72636cf2fdd0b06ff49104b4158b427b3c7d9
Harry Newman <h.newman2005@outlook.com>
2025-07-05 18:01:04 +0100

    Added print_vector(): prints vector

--- data structures/python/vector/vector.py
+++ data structures/python/vector/vector.py
@@ -1,10 +1,22 @@
 class Vector:
-    def __init__(self, data, currentSize=0, capacity=0):
-    if data is None:
-        self.data = []
-    else:
-        self.data = data
-        
-    self.currentSize = 0
-    self.capacity = 0
+    def __init__(self, data=None):
+        if data is None:
+            self.data = []
+        else:
+            self.data = data
 
+        self.currentSize = len(self.data)
+        self.capacity = len(self.data)
+
+    def print_vector(self):
+        for x in self.data:
+            print(x)
+
+def main():
+    data = [3, 5, 9]
+    v = Vector(data)
+
+    v.print_vector()
+
+if __name__ == "__main__":
+    main()
