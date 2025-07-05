01ee5a1e66b31b4a08dc6e6d9767731754d0aac7
Harry Newman <h.newman2005@outlook.com>
2025-07-05 17:32:20 +0100

    Changed this to now be the main app area (vector.c & vector.h should be untouched)

---
+++ data structures/c/vector/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include "vector.h"
+
+int main() {
+  Vector v = createVector();
+
+  // Push some elements
+  pushBack(&v, 1);
+  pushBack(&v, 2);
+  pushBack(&v, 3);
+  printf("Before resize: ");
+  printVector(&v); // Should print 1, 2, 3
+  printf("Size: %d, Capacity: %d\n", v.currentSize, v.capacity);
+
+  // Resize down to 2 elements
+  resize(&v, 2);
+  printf("After resizing down to 2: ");
+  printVector(&v); // Should print 1, 2
+  printf("Size: %d, Capacity: %d\n", v.currentSize, v.capacity);
+
+  // Resize up to 5 elements (new elements zero-initialized)
+  resize(&v, 5);
+  printf("After resizing up to 5: ");
+  printVector(&v); // Should print 1, 2, 0, 0, 0
+  printf("Size: %d, Capacity: %d\n", v.currentSize, v.capacity);
+
+  free(v.data);
+  return 0;
+}
