55e13d90821a50074096fd0d3edadad0f8698a73
Harry Newman <h.newman2005@outlook.com>
2025-07-05 16:04:23 +0100

    added push_back -> append a new element to the end of the vector

--- data structures/c/vector/vector.c
+++ data structures/c/vector/vector.c
@@ -1,18 +1,26 @@
-#include <stdlib.h>
 typedef struct {
   int *data;
   int currentSize;
   int capacity;
 } Vector;
 
+// Returns 1 if hte list is empty, otherwise 0
 int isEmpty(const Vector *v) {
   return v->currentSize == 0;
 }
 
+// Return number of elements in the vector
 int size(const Vector *v) {
   return v->currentSize;
 }
 
+// Remove all elements from vector
 void clear(Vector *v) {
   v->currentSize = 0;
+}
+
+// Append a new element to the end of the vector
+void push_back(Vector *v, int value) {
+  v->data[v->currentSize] = value;
+  v->currentSize++;
 }
