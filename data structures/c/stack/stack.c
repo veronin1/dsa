57ea47b82e097ae1a5a8b716b79c36a3f225e2ec
Harry Newman <h.newman2005@outlook.com>
2025-07-05 23:00:04 +0100

    createStack function

--- data structures/c/stack/stack.c
+++ data structures/c/stack/stack.c
@@ -1,4 +1,22 @@
+#include <stdlib.h>
+
 typedef struct {
-  int *arr;
+  int *data;
   int top;
 } Stack;
+
+Stack createStack(void) {
+  Stack s;
+  s.data = NULL;
+  s.top = 0;
+  return s;
+}
+
+int pop(Stack *s) {
+  // if stack empty
+  if (!s || s->top == 0) {
+    return -1;
+  }
+  s->top--;
+  return s->data[s->top];
+}
