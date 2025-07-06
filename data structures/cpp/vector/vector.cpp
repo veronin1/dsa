65a06792ab0f07f4579fd869d00f0495361690c9
Harry Newman <h.newman2005@outlook.com>
2025-07-06 14:02:21 +0100

    front(): return first element in vector, throws out of range exception if vect empty

--- data structures/cpp/vector/vector.cpp
+++ data structures/cpp/vector/vector.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <stdexcept>
 
 class Vector {
  private:
@@ -9,10 +10,19 @@ class Vector {
  public:
   Vector() : currentSize(0), capacity(0) {}
 
+  // returns currentSize
   int size() const { return currentSize; }
 
   // return true if no elems
   bool empty() const { return currentSize == 0; }
+
+  // return first element in vect
+  int front() {
+    if (currentSize == 0) {
+      throw std::out_of_range("Vector is empty");
+    }
+    return data[0];
+  }
 
  private:
 };
