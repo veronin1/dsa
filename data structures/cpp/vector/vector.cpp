40d2a52f7321b9354d8c00261d8e006c5841c544
Harry Newman <h.newman2005@outlook.com>
2025-07-06 13:54:34 +0100

    Made class blueprint

--- data structures/cpp/vector/vector.cpp
+++ data structures/cpp/vector/vector.cpp
@@ -1,1 +1,11 @@
-#include <iostream>
+class Vector {
+ private:
+  int *data;
+  int currentSize;
+  int capacity;
+
+ public:
+  Vector() : data(nullptr), currentSize(0), capacity(0) {}
+
+  int size(void) { return currentSize; }
+};
