e96b8a1cdad3373b57ab5f0e3fae32efdd878ba6
Harry Newman <h.newman2005@outlook.com>
2025-07-24 15:01:20 +0100

    createQueue

---
+++ data structures/c/queue/queue.c
@@ -0,0 +1,18 @@
+#include "queue.h"
+
+Queue createQueue(size_t capacity) {
+  Queue q;
+
+  if (capacity == 0) {
+    capacity = 1000;
+  }
+  q.front = 0;
+
+  q.capacity = capacity;
+  q.data = malloc(sizeof(int) * q.capacity);
+  if (q.data == NULL) {
+    q.capacity = 0;
+    return q;
+  }
+  return q;
+}
