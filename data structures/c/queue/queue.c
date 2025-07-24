ae45ecbd8d8fce7b310e60aef8fd56434f1ad588
Harry Newman <h.newman2005@outlook.com>
2025-07-24 15:09:59 +0100

    enqueue() finished

--- data structures/c/queue/queue.c
+++ data structures/c/queue/queue.c
@@ -7,7 +7,7 @@ Queue createQueue(size_t capacity) {
     capacity = 1000;
   }
   q.front = 0;
-
+  q.rear = 0;
   q.capacity = capacity;
   q.data = malloc(sizeof(int) * q.capacity);
   if (q.data == NULL) {
@@ -23,4 +23,15 @@ bool isEmpty(Queue *q) {
 
 size_t size(Queue *q) {
   return q->size;
+}
+
+void enqueue(Queue *q, int element) {
+  if (q->size == q->capacity) {
+    // queue full
+    return;
+  }
+
+  q->data[q->rear] = element;
+  q->rear = (q->rear + 1) % q->capacity;
+  q->size++;
 }
