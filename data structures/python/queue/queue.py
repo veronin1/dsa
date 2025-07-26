b2d9cb647a42a5998f099a5cf8794807d1bdf36c
Harry Newman <h.newman2005@outlook.com>
2025-07-26 20:19:27 +0100

    Finish queue in python

--- data structures/python/queue/queue.py
+++ data structures/python/queue/queue.py
@@ -7,17 +7,34 @@ class Queue:
         self.maxSize = maxSize
 
     def enqueue(self, value):
-        if self.currentSize == self.maxSize
+        if self.currentSize == self.maxSize:
             raise OverflowError("Queue is full")
     
         self.tail = (self.tail + 1) % self.maxSize
-        self.data[self.tail]
+        self.data[self.tail] = value
         self.currentSize += 1
 
     def dequeue(self):
         if self.isEmpty():
             raise IndexError("Queue is empty")
         front = self.data[self.head]
-        del self.data[self.head]
+        self.data[self.head] = None
+        self.head = (self.head + 1) % self.maxSize
         self.currentSize -= 1
         return front
+
+    def front(self):
+        if self.isEmpty():
+            raise IndexError("Queue is empty")
+        return self.data[self.head]
+
+    def back(self):
+        if self.isEmpty():
+            raise IndexError("Queue is empty")
+        return self.data[self.tail]
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def size(self):
+        return self.currentSize
