d86653c28a1871e34fe2b3e4c3b465ba537db678
Harry Newman <h.newman2005@outlook.com>
2025-07-27 00:54:43 +0100

    [fix] minor issues

--- data structures/python/stack/stack.py
+++ data structures/python/stack/stack.py
@@ -1,18 +1,18 @@
 class Stack:
     def __init__(self, capacity):
-        self.data = [None] * maxSize
+        self.data = [None] * capacity
         self.capacity = capacity
         self.current_size = 0
 
     def push(self, element):
-        if is_full:
+        if self.is_full():
             raise OverflowError("Stack is full")
 
         self.data[self.current_size] = element
         self.current_size += 1
 
     def pop(self):
-        if is_empty:
+        if self.is_empty():
             raise IndexError("Stack empty")
         self.current_size -= 1
         return self.data[self.current_size]
