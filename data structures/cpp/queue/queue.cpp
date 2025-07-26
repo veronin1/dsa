f4beaa2e945432a4dd31f3523677624d9d9d8519
Harry Newman <h.newman2005@outlook.com>
2025-07-26 19:07:35 +0100

    Refactored

--- data structures/cpp/queue/queue.cpp
+++ data structures/cpp/queue/queue.cpp
@@ -1,33 +1,26 @@
-#include <cstddef>
+#include "queue.hpp"
 
 #define DEFAULT_INIT_SIZE 1000
 
-class Queue {
- private:
-  int *data;
-  size_t head;
-  size_t tail;
-  size_t size;
-  size_t capacity;
+Queue::Queue(size_t cap) {
+  capacity = cap;
+  data = new int[capacity];
+  head = 0;
+  tail = capacity - 1;
+  size = 0;
+}
 
- public:
-  Queue(size_t cap) {
-    capacity = cap;
-    data = new int[capacity];
-    head = 0;
-    tail = capacity - 1;
-    size = 0;
+Queue::~Queue() {
+  delete[] data;
+}
+
+void Queue::enqueue(int element) {
+  if (size == capacity) {
+    expand();
   }
+  data[(++tail) % capacity] = element;
+  ++size;
+}
 
-  ~Queue() { delete[] data; }
-
-  void enqueue(int element) {
-    if (size == capacity) {
-      expand();
-    }
-    data[(++tail) % capacity] = element;
-    ++size;
-  }
-
-  void expand();
-};
+void Queue::expand() {
+}
