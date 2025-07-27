04e9f7edaec742b2421e0003b953c76d20855dd5
Harry Newman <h.newman2005@outlook.com>
2025-07-28 00:09:40 +0100

    Added helper functions for getting indexes of certain parts of the vector (parent & children)

--- data structures/cpp/heap/heap.hpp
+++ data structures/cpp/heap/heap.hpp
@@ -4,4 +4,22 @@ template <typename T>
 class Heap {
  private:
   std::vector<T> *data;
+  size_t parentIdx(size_t index);
+  size_t leftNodeIdx(size_t index);
+  size_t rightNodeIdx(size_t index);
 };
+
+template <typename T>
+size_t Heap<T>::parentIdx(size_t index) {
+  return index / 2;
+}
+
+template <typename T>
+size_t Heap<T>::leftNodeIdx(size_t index) {
+  return index * 2 + 1;
+}
+
+template <typename T>
+size_t Heap<T>::rightNodeIdx(size_t index) {
+  return index * 2 + 2;
+}
