6b9ea51bf7f287b7534f0e1c29755d0f4623e779
Harry Newman <h.newman2005@outlook.com>
2025-07-27 15:38:23 +0100

    Added types for the struct to define if its a minheap or maxheap and added a heapifyup function

--- data structures/c/heap/heap.c
+++ data structures/c/heap/heap.c
@@ -1,13 +1,16 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+typedef enum { MIN_HEAP, MAX_HEAP } HeapType;
+
 typedef struct {
   void **data;
   size_t size;
   size_t capacity;
+  HeapType type;
 } Heap;
 
-Heap *createHeap(size_t capacity) {
+Heap *createHeap(size_t capacity, HeapType type) {
   Heap *heap = malloc(sizeof(Heap));
   if (!heap) {
     return NULL;
@@ -21,6 +24,7 @@ Heap *createHeap(size_t capacity) {
 
   heap->size = 0;
   heap->capacity = capacity;
+  heap->type = type;
   return heap;
 }
 
@@ -47,8 +51,33 @@ void resize(Heap *heap) {
   heap->capacity = newCapacity;
 }
 
-void heapifyUp(Heap *heap, size_t size);
+// restore heap property after inserting a new element
+void heapifyUp(Heap *heap, size_t index) {
+  if (index == 0) {
+    return;
+  }
 
+  while (index > 0) {
+    int parent = (index - 1) / 2;
+    if (heap->type == MAX_HEAP) {
+      if (heap->data[index] > heap->data[parent]) {
+        void *tmp;
+        heap->data[parent] = tmp;
+        heap->data[parent] = heap->data[index];
+        heap->data[index] = tmp;
+        index = parent;
+      }
+    } else if (heap->type == MIN_HEAP) {
+      if (heap->data[index] < heap->data[parent]) {
+        void *tmp;
+        heap->data[parent] = tmp;
+        heap->data[parent] = heap->data[index];
+        heap->data[index] = tmp;
+        index = parent;
+      }
+    }
+  }
+}
 // adds the element to top of the heap
 void push(Heap *heap, const void *val) {
   if (heap->size == heap->capacity) {
