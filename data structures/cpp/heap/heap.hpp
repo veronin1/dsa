3711f808ab20c990095a5d0b3b0da1fcc08d3709
Harry Newman <h.newman2005@outlook.com>
2025-07-28 00:13:44 +0100

    Created constructor & destructor

--- data structures/cpp/heap/heap.hpp
+++ data structures/cpp/heap/heap.hpp
@@ -7,6 +7,10 @@ class Heap {
   size_t parentIdx(size_t index);
   size_t leftNodeIdx(size_t index);
   size_t rightNodeIdx(size_t index);
+
+ public:
+  Heap();
+  ~Heap();
 };
 
 template <typename T>
@@ -22,4 +26,14 @@ size_t Heap<T>::leftNodeIdx(size_t index) {
 template <typename T>
 size_t Heap<T>::rightNodeIdx(size_t index) {
   return index * 2 + 2;
+}
+
+template <typename T>
+Heap<T>::Heap() {
+  data = new std::vector<T>();
+}
+
+template <typename T>
+Heap<T>::~Heap() {
+  delete data;
 }
