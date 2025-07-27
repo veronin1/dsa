ac754ab4f6cafb56274789d349478f6660e68aa4
Harry Newman <h.newman2005@outlook.com>
2025-07-28 00:52:44 +0100

    Created functions for checking if its a minheap or a maxheap

--- data structures/cpp/heap/heap.hpp
+++ data structures/cpp/heap/heap.hpp
@@ -3,7 +3,7 @@
 template <typename T>
 class Heap {
  private:
-  std::vector<T> *data;
+  std::vector<T> data;
   size_t parentIdx(size_t index);
   size_t leftNodeIdx(size_t index);
   size_t rightNodeIdx(size_t index);
@@ -11,11 +11,15 @@ class Heap {
  public:
   Heap();
   ~Heap();
+
+  void push(const T &val);
+  bool isMaxHeap();
+  bool isMinHeap();
 };
 
 template <typename T>
 size_t Heap<T>::parentIdx(size_t index) {
-  return index / 2;
+  return (index - 1) / 2;
 }
 
 template <typename T>
@@ -36,4 +40,42 @@ Heap<T>::Heap() {
 template <typename T>
 Heap<T>::~Heap() {
   delete data;
+}
+
+template <typename T>
+void Heap<T>::push(const T &val) {
+  data.push_back(val);
+  size_t index = data.size() - 1;
+}
+
+template <typename T>
+bool Heap<T>::isMaxHeap() {
+  for (size_t i = 0; i < data.size(); ++i) {
+    size_t left = leftNodeIdx(i);
+    size_t right = rightNodeIdx(i);
+
+    if (left < data.size() && data[i] < data[left]) {
+      return false;
+    }
+    if (right < data.size() && data[i] < data[right]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+template <typename T>
+bool Heap<T>::isMinHeap() {
+  for (size_t i = 0; i < data.size(); ++i) {
+    size_t left = leftNodeIdx(i);
+    size_t right = rightNodeIdx(i);
+
+    if (left < data.size() && data[left] > data[i]) {
+      return false;
+    }
+    if (right < data.size() && data[right] > data[i]) {
+      return false;
+    }
+  }
+  return true;
 }
