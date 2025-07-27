90c56ff9b5bb5350c7535a613b1cb135900b2d1f
Harry Newman <h.newman2005@outlook.com>
2025-07-27 14:52:23 +0100

    Created createHeap function to dynamically create a heap, added blueprint for resize, heapifyUp and implemented push function

--- data structures/c/heap/heap.c
+++ data structures/c/heap/heap.c
@@ -1,7 +1,40 @@
 #include <stddef.h>
+#include <stdlib.h>
 
 typedef struct {
-  int *data;
+  void **data;
   size_t size;
   size_t capacity;
 } Heap;
+
+Heap *createHeap(size_t capacity) {
+  Heap *heap = malloc(sizeof(Heap));
+  if (!heap) {
+    return NULL;
+  }
+
+  heap->data = malloc(capacity * sizeof(void *));
+  if (!heap->data) {
+    free(heap);
+    return NULL;
+  }
+
+  heap->size = 0;
+  heap->capacity = capacity;
+  return heap;
+}
+
+void resize(Heap *heap);
+
+void heapifyUp(Heap *heap, size_t size);
+
+// adds the element to top of the heap
+void push(Heap *heap, const void *val) {
+  if (heap->size == heap->capacity) {
+    resize(heap);
+  }
+
+  heap->data[heap->size] = (void *) val;
+  heapifyUp(heap, heap->size);
+  heap->size++;
+}
