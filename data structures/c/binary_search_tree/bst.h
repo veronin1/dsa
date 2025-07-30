91d42918a70c122fa0f833ead2b1120e444df512
Harry Newman <h.newman2005@outlook.com>
2025-07-30 23:03:51 +0100

    Added other functions to header

--- data structures/c/binary_search_tree/bst.h
+++ data structures/c/binary_search_tree/bst.h
@@ -1,6 +1,7 @@
 #ifndef BST_H
 #define BST_H
 
+#include <stdbool.h>
 #include <stddef.h>
 
 typedef struct Node {
@@ -14,8 +15,18 @@ typedef struct {
   size_t size;
 } BinarySearchTree;
 
-BinarySearchTree *createTree(const size_t size);
-void insert(BinarySearchTree *tree, const int element);
-TreeNode *insert_node(TreeNode *node, int value);
+BinarySearchTree *createTree(void);
+
+void insert(BinarySearchTree *tree, int value);
+void remove_node(BinarySearchTree *tree, int value);
+bool contains(const BinarySearchTree *tree, int value);
+void clear(BinarySearchTree *tree);
+int height(const BinarySearchTree *tree);
+size_t size(const BinarySearchTree *tree);
+bool empty(const BinarySearchTree *tree);
+
+int *in_order_traversal(const BinarySearchTree *tree, size_t *out_size);
+int *pre_order_traversal(const BinarySearchTree *tree, size_t *out_size);
+int *post_order_traversal(const BinarySearchTree *tree, size_t *out_size);
 
 #endif
